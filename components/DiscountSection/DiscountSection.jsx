db09927efce81a7a9c0c8c3da40e3b116bda0605
Дмитрий Кубарский <dzmitrykubarski@gmail.com>
2021-02-04 22:42:59 +0300

    Create component DiscountSection

---
+++ components/DiscountSection/DiscountSection.jsx
@@ -0,0 +1,76 @@
+// libs
+import styled from 'styled-components'
+
+// utils
+import { discountItems } from './discountItems'
+
+// components
+import { Container } from './../UI/Container/Container'
+import Button from './../UI/Button/Button'
+import Card from './../UI/Card/Card'
+
+const DiscountSection = () => {
+    return (
+        <Wrapper>
+            <Container>
+                <Title>
+                    Постоянные акции
+                    <span>и сезонные предложения</span>
+                </Title>
+
+                <CardList>
+                    {discountItems.map((item, index) =>
+                        <CardWrap key={`${item.imgUrl}_${index}`}>
+                            <Card variant='limited' item={item} />
+                        </CardWrap>
+                    )}
+                </CardList>
+
+                <BtnWrap>
+                    <Button large full>Смотреть больше моделей</Button>
+                </BtnWrap>
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default DiscountSection
+
+// styles
+const Wrapper = styled.section`
+    padding: 100px 0px;
+`
+
+const Title = styled.h2`
+    font-weight: 300;
+    font-size: 44px;
+    color: #535353;
+    text-align: center;
+    margin-bottom: 50px;
+
+    span {
+        display: block;
+        font-weight: 700;
+        font-size: 44px;
+        color: #8b75c8;
+    }
+`
+const CardList = styled.div`
+    display: flex;
+    margin-left: -15px;
+    margin-right: -15px;  
+    margin-bottom: 30px;
+`
+
+const CardWrap = styled.div`
+    max-width: calc(25% - 30px);
+    margin-bottom: 30px;
+    margin-left: 15px;
+    margin-right: 15px;
+`
+
+const BtnWrap = styled.div`
+    width: 100%;
+    max-width: 343px;
+    margin: 0 auto;
+`
---
+++ components/DiscountSection/discountItems.js
@@ -0,0 +1,29 @@
+export const discountItems = [
+    {
+        title: 'Наушники Bluetooth Baseus Encok D01 NGD01-09 (red)',
+        discountCount: '20',
+        imgUrl: '/discount/item-1.jpg',
+        daysCount: '7'
+    },
+
+    {
+        title: 'Наушники Bluetooth Baseus Encok D01 NGD01-04 (green)',
+        discountCount: '35',
+        imgUrl: '/discount/item-2.jpg',
+        daysCount: '14'
+    },
+
+    {
+        title: 'Наушники Bluetooth Baseus Encok D05 NGD01-05 (black)',
+        discountCount: '50',
+        imgUrl: '/discount/item-3.jpg',
+        daysCount: '22'
+    },
+
+    {
+        title: 'Наушники Bluetooth Baseus Encok D07 NGD01-07 (red)',
+        discountCount: '15',
+        imgUrl: '/discount/item-1.jpg',
+        daysCount: '50'
+    },
+]
